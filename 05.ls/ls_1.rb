849657744d304da58c73c678b889bada8cff175e
iwamurahiroshi <hiroshi.kurara.m@gmail.com>
2021-09-11 11:01:17 +0900

    行数を変更できるように訂正

--- 05.ls/ls_1.rb
+++ 05.ls/ls_1.rb
@@ -1,19 +1,22 @@
 # frozen_string_literal: true
 
 files = Dir.glob('*')
+
 column = 3
 extra = files.length % column
-(3 - extra).times do
+(column - extra).times do
   files << nil
 end
-
 sliced_files = files.each_slice(files.length / column).to_a
 longest_word_per_column = sliced_files.map { |x| x.max_by { |y| y.to_s.length } }
 longest_wordsize_per_column = longest_word_per_column.map(&:length)
-
 transposed_files = sliced_files.transpose
-transposed_files.each do |file|
-  print file[0].ljust(longest_wordsize_per_column[0] + 5)
-  print file[1].ljust(longest_wordsize_per_column[1] + 5)
-  puts file[2]&.ljust(longest_wordsize_per_column[2] + 5)
+transposed_files.each do |files_per_column|
+  files_per_column.each.with_index(1) do |file, i|
+    if i == column
+      puts file&.ljust(longest_wordsize_per_column[i - 1] + 3)
+    else
+      print file&.ljust(longest_wordsize_per_column[i - 1] + 3)
+    end
+  end
 end
