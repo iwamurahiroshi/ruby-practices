d75590d6a34180d68566878294b061d9de49ea26
iwamurahiroshi <hiroshi.kurara.m@gmail.com>
2021-09-21 16:21:33 +0900

    -rオプション付lsコマンドの提出

---
+++ 05.ls/ls_3.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+files = Dir.glob('*')
+opt = OptionParser.new
+opt.on('-r') { files = files.reverse }
+opt.parse!(ARGV)
+
+column = 3
+extra = files.length % column
+(column - extra).times do
+  files << nil
+end
+sliced_files = files.each_slice(files.length / column).to_a
+longest_word_per_column = sliced_files.map { |x| x.max_by { |y| y.to_s.length } }
+longest_wordsize_per_column = longest_word_per_column.map(&:length)
+transposed_files = sliced_files.transpose
+transposed_files.each do |files_per_column|
+  files_per_column.each.with_index(1) do |file, i|
+    if i == column
+      puts file&.ljust(longest_wordsize_per_column[i - 1] + 3)
+    else
+      print file&.ljust(longest_wordsize_per_column[i - 1] + 3)
+    end
+  end
+end
